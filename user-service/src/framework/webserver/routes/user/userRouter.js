596920d2aa811ec146f8b1749d80ae3f807672e2
Navaneeth V <navaneethvinod18@gmail.com>
2024-07-03 20:56:37 +0530

    User Service : login complete, code optimised for usecases

--- user-service/src/framework/webserver/routes/user/userRouter.js
+++ user-service/src/framework/webserver/routes/user/userRouter.js
@@ -1,15 +1,20 @@
-import userController from '../../../../adapters/controllers/user/userController.js'
-import userRepositoryImp from '../../../database/mongodb/repositories/user/userRepositoryImp.js'
-import userRepositoryInt from '../../../../application/repositories/user/userRepsitoryInt.js'
-import userServiceImp from '../../../service/user/userServiceImp.js'
-import userServiceInt from '../../../../application/services/user/userServiceInt.js'
+import userController from "../../../../adapters/controllers/user/userController.js";
+import userRepositoryImp from "../../../database/mongodb/repositories/user/userRepositoryImp.js";
+import userRepositoryInt from "../../../../application/repositories/user/userRepsitoryInt.js";
+import userServiceImp from "../../../service/user/userServiceImp.js";
+import userServiceInt from "../../../../application/services/user/userServiceInt.js";
 
 const userRouter = (express) => {
-    const router = express.Router()
-    const controller = userController(userRepositoryInt, userRepositoryImp, userServiceInt, userServiceImp)
-    router.route('/register').post(controller.createUser)
-    router.route('/login').post(controller.login)
-    return router
-}
+  const router = express.Router();
+  const controller = userController(
+    userRepositoryInt,
+    userRepositoryImp,
+    userServiceInt,
+    userServiceImp
+  );
+  router.route("/register").post(controller.createUser);
+  router.route("/login").post(controller.login);
+  return router;
+};
 
-export default userRouter
+export default userRouter;
